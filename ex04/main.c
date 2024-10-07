257b5ce51035155855ea6822985075f50420132f
Tsilavina Gaettan Andriamiharison <tsilandr@c1r7s3.42antananarivo.mg>
2024-10-07 07:41:05 +0300

    add test 4

---
+++ ex04/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int	ft_str_is_lowercase(char *str);
+
+int main(void)
+{
+	char	str1[] = "Hello 42 World";
+	char	str2[] = "helloworld";
+	char	str3[] = "HelloWorld";
+	char	str4[] = "";
+
+	
+	printf("\nValeur de str1 AVANT de de passer par fonction: '%s'", str1);
+	printf("\nValeur de str2 AVANT de de passer par fonction: '%s'", str2);
+	printf("\nValeur de str3 AVANT de de passer par fonction: '%s'", str3);
+	printf("\nValeur de str4 AVANT de de passer par fonction: '%s'\n\n", str4);
+
+	int res1 = ft_str_is_lowercase(str1);
+	int res2 = ft_str_is_lowercase(str2);
+	int res3 = ft_str_is_lowercase(str3);
+	int res4 = ft_str_is_lowercase(str4);
+
+
+	printf("ft_str_is_lowercase(str1) = %d\n", res1);
+	printf("ft_str_is_lowercase(str2) = %d\n", res2);
+	printf("ft_str_is_lowercase(str3) = %d\n", res3);
+	printf("ft_str_is_lowercase(str4) = %d\n", res4);
+
+	return (0);
+}
